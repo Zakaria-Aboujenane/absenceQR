46447965dbcc83fabcd6a620b99b480f0ec859e0
AHMED.DEVPANI <chahidahmed535@gmail.com>
2022-06-15 17:36:23 +0100

    prof account

--- resources/views/qrcodepage.blade.php
+++ resources/views/qrcodepage.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.prof')
 @section('content')
+
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Séances</h1>
+        <h1 class="h3 mb-0 text-gray-800">Scanner le QR CODE</h1>
     </div>
+    <div class="card shadow mb-4">
+    <div class="card-body">
+        <div id="qrCodePlace" > {{ $qrcode }} </div>
 
-  <div id="qrCodePlace" > {{ $qrcode }} </div>
-
-  <form action="/fin_seance" method="GET">
-      <input type="hidden" name="id_seance" value="{{$id_Seance}}">
-      <input type="hidden" name="id_filiere" value="{{$id_filiere}}">
-      <button type="submit">Declarer le debut de seance (fin de marquage d'absence)</button>
-  </form>
+        <form action="/fin_seance" method="GET">
+            <input type="hidden" name="id_seance" value="{{$id_Seance}}">
+            <input type="hidden" name="id_filiere" value="{{$id_filiere}}">
+            <button type="submit" class="btn btn-primary btn-user float-right">Fin de marquage d'absence</button>
+        </form>
+    </div>
+    </div>
 
 <script>
     function qr_code_generation() {
