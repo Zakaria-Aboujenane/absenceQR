e6904ae72260a6749047863f9647a81a389b6aad
AHMED.DEVPANI <chahidahmed535@gmail.com>
2022-06-15 17:52:36 +0100

    'hh'

--- resources/views/layouts/prof.blade.php
+++ resources/views/layouts/prof.blade.php
@@ -103,13 +103,7 @@
         <!-- /.container-fluid -->
 
     </div>
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2022</span>
-          </div>
-        </div>
-      </footer>
+     
       <!-- End of Footer -->
 
     </div>
--- resources/views/qrcodepage.blade.php
+++ resources/views/qrcodepage.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class="card shadow mb-4">
     <div class="card-body">
-        <div id="qrCodePlace" > {{ $qrcode }} </div>
+        <center><div id="qrCodePlace" > {{ $qrcode }} </div></center>
 
         <form action="/fin_seance" method="GET">
             <input type="hidden" name="id_seance" value="{{$id_Seance}}">
